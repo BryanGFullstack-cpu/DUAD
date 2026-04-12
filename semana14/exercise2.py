5087afeca7d1526903f12f8b0cda72c583e0cc0e
BryanGFullstack-cpu <BryanGFullstack@icloud.com>
2026-04-12 16:15:56 -0700

    added exercise 2

---
+++ semana14/exercise2.py
@@ -0,0 +1,19 @@
+#Validate number
+
+def validate_numbers(func):
+    def wrapper(*args, **kwargs):
+        for value in list(args) + list(kwargs.values()):
+            if not isinstance(value, (int, float)):
+                raise TypeError("All parameters must be numbers")
+        return func(*args, **kwargs)
+    return wrapper
+
+# decorador
+@validate_numbers
+def subtract(a, b):
+    return a - b
+
+
+#Ejemplo de uso
+if __name__ == "__main__":
+    print(subtract(10, 4))
