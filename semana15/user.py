da86376b66e628b36463099c4c68f8c51a01e43f
BryanGFullstack-cpu <BryanGFullstack@icloud.com>
2026-04-26 18:01:29 -0700

    User corregido

--- semana15/user.py
+++ semana15/user.py
@@ -6,16 +6,24 @@ user_logged_in = False
 def require_login(func):
     def wrapper(*args, **kwargs):
         if not user_logged_in:
-            print("User must be logged in to access this function.")
-            return None
+            raise Exception("Usuario no autenticado")
         return func(*args, **kwargs)
     return wrapper
-
 
 @require_login
 def view_profile():
     print("Viewing user profile...")
 
-    view_profile
 
-    
+    #para probar hare estos dos try
+
+
+try:
+    view_profile()
+except Exception as e:
+    print(e)
+
+    user_logged_in = True
+view_profile()
+
+
